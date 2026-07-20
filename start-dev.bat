65d771a145a207cd9c882d44bd470fd63a30433c
arianna <arianna.comi03@gmail.com>
2026-07-20 12:58:43 +0200

    fix script error

--- start-dev.bat
+++ start-dev.bat
@@ -4,7 +4,7 @@ cd /d %~dp0
 
 call build.bat || exit /b %ERRORLEVEL%
 
-cd docker | exit /b %ERRORLEVEL%
+cd docker || exit /b %ERRORLEVEL%
 
 docker compose -p dev --profile dev up -d || exit /b %ERRORLEVEL%
 
