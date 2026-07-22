c2ef60201ed551ddd4e98bc6c2b27b07ca5dbdc5
arianna <arianna.comi03@gmail.com>
2026-07-22 10:27:39 +0200

    feat(db): configure fFyway (user and loan services)

--- loan-service/src/main/resources/db/migration/V1__initial_schema.sql
+++ loan-service/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,6 +1,4 @@
-use user_db;
-
-CREATE TABLE IF NOT EXISTS user_events (
+CREATE TABLE IF NOT EXISTS loan_events (
     id bigint not null auto_increment,
     aggregate_id varchar(100) not null,
     aggregate_type varchar(50) not null,
@@ -21,13 +19,22 @@ CREATE TABLE IF NOT EXISTS user_events (
     key idx_event_type (event_type)
 );
 
+CREATE TABLE IF NOT EXISTS loan_view (
+  id varchar(36) not null,
+  isbn varchar(17) not null,
+  user_id varchar(36) not null,
+  start_date date not null,
+  end_date date not null,
+  status ENUM('PENDING', 'RESERVED', 'CONFIRMED', 'CANCELED', 'RETURNED', 'FAILED') not null default 'PENDING',
+  created_at timestamp not null default current_timestamp,
+  updated_at timestamp not null default current_timestamp,
+  primary key(id)
+);
+
 CREATE TABLE IF NOT EXISTS user_view(
 	id varchar(36) not null,
 	email varchar(100) not null,
-	name varchar(50) not null,
-	lastname varchar(50) not null,
 	user_identity_provider_id varchar(100) not null,
-	role ENUM('READER', 'LIBRARIAN', 'ADMIN') not null default 'READER',
 	status ENUM('ACTIVE', 'SUSPENDED', 'DISABLED') not null default 'ACTIVE',
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp,
--- user-service/src/main/resources/db/migration/V1__initial_schema.sql
+++ user-service/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,42 +1,31 @@
-use loan_db;
-
-CREATE TABLE IF NOT EXISTS loan_events (
+CREATE TABLE IF NOT EXISTS user_events (
     id bigint not null auto_increment,
     aggregate_id varchar(100) not null,
-    aggregate_type varchar(50) not null,
+	aggregate_type varchar(50) not null,
     event_type varchar(50) not null,
     event_id char(36) NOT NULL,
     event_version int not null,
-    schema_version int,
-    event_category ENUM('PRODUCER', 'CONSUMER') not null default 'PRODUCER',
+	schema_version int,
+	event_category ENUM('PRODUCER', 'CONSUMER') not null default 'PRODUCER',
     payload json not null,
-    processed boolean not null default false,
-    failed boolean not null default false,
+	processed boolean not null default false,
+	failed boolean not null default false,
     retry_count int not null default 0,
     occurred_at timestamp not null,
     primary key (id),
     unique key uq_aggregate_version (aggregate_id, event_version),
-    unique key uq_aggregateType_version (event_id, aggregate_type),
+	unique key uq_aggregateType_version (event_id, aggregate_type),
     key idx_aggregate (aggregate_id),
     key idx_event_type (event_type)
-);
-
-CREATE TABLE IF NOT EXISTS loan_view (
-  id varchar(36) not null,
-  isbn varchar(17) not null,
-  user_id varchar(36) not null,
-  start_date date not null,
-  end_date date not null,
-  status ENUM('PENDING', 'RESERVED', 'CONFIRMED', 'CANCELED', 'RETURNED', 'FAILED') not null default 'PENDING',
-  created_at timestamp not null default current_timestamp,
-  updated_at timestamp not null default current_timestamp,
-  primary key(id)
 );
 
 CREATE TABLE IF NOT EXISTS user_view(
 	id varchar(36) not null,
 	email varchar(100) not null,
+	name varchar(50) not null,
+	lastname varchar(50) not null,
 	user_identity_provider_id varchar(100) not null,
+	role ENUM('READER', 'LIBRARIAN', 'ADMIN') not null default 'READER',
 	status ENUM('ACTIVE', 'SUSPENDED', 'DISABLED') not null default 'ACTIVE',
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp,
